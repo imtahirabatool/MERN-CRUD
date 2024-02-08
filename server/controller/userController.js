77811eecff1de21fdc888ed36bffc342ff009597
im-TahiraBatool <btahira183@gmail.com>
2024-02-09 00:35:53 +0500

    first commit

---
+++ server/controller/userController.js
@@ -0,0 +1,75 @@
+import User from "../models/userModel.js";
+
+export const create = async (req, res) => {
+  try {
+    const userData = new User(req.body);
+
+    if (!userData) {
+      return res.status(404).json({ msg: "User data not found!" });
+    }
+    const savedData = await userData.save();
+    res.status(200).json({
+      success: true,
+      message: "User created successfully!",
+      savedData,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const userData = await User.find();
+    if (!userData) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+
+    const updatedData = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "User updated successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ msg: "User deleted successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodeParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import route from "./routes/userRoutes.js";
+
+const app = express();
+app.use(bodeParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const URL = process.env.MONGOURL;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("DB connected successfully!");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port:${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.use("/api", route);
